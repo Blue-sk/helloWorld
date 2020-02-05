89473bb1c187ddc4920aed1603f74941283bc810
李寒畅 <3547890397@qq.com>
2020-02-05 21:46:16 +0800

    更新2

--- src/hello/MyhelloWorld.java
+++ src/hello/MyhelloWorld.java
@@ -4,5 +4,6 @@ public class MyhelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
         System.out.println("Hello git");
+        System.out.println("我是新加进来的");
     }
 }
